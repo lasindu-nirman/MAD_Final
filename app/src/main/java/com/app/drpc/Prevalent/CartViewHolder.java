85559ece3ef75c20d9d9d01d036cdbc03fed455a
lasindu-nirman <53116333+lasindu-nirman@users.noreply.github.com>
2020-10-04 18:53:38 +0530

    Add files via upload

---
+++ app/src/main/java/com/app/drpc/Prevalent/CartViewHolder.java
@@ -0,0 +1,29 @@
+package com.app.drpc.Prevalent;
+
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.app.drpc.Interface.ItemClickListner;
+import com.app.drpc.R;
+
+public class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public TextView txtProductName,txtProductPrice,txtProductQuantity;
+    private ItemClickListner itemClickListner;
+
+    public CartViewHolder(View itemView) {
+        super(itemView);
+        txtProductName = itemView.findViewById(R.id.cart_product_name);
+        txtProductPrice = itemView.findViewById(R.id.cart_product_price);
+        txtProductQuantity = itemView.findViewById(R.id.cart_product_quantity);
+    }
+
+    @Override
+    public void onClick(View view) {
+        itemClickListner.onClick(view,getAdapterPosition(),false);
+    }
+
+    public void setItemClickListner(ItemClickListner itemClickListner) {
+        this.itemClickListner = itemClickListner;
+    }
+}
